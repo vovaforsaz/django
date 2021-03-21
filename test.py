6e71760d2fe26a758cd369119643cee14bbf7af9
vova <vovaforsaz@gmail.com>
2021-03-21 16:00:45 +0200

    test django

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('a')
+M2 = [[-1, 2, 3, 4],
+          [5, -6, 7, 8],
+          [5, 6, 5, 8],
+          [1, 6, 7, 11]]
+a = []
+for i in range(len(M2)):
+    for j in range(len(M2[i])):
+         if i == j:
+            a.append(M2[i][j])
+ab = {
+    'sd':23
+}
+print(ab['sd'],'test')
