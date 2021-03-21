c947155ae3341bf051b8f40923418b39fe701209
vova <vovaforsaz@gmail.com>
2021-03-21 17:08:43 +0200

    test django

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', include('mainApp.urls')),
     url(r'^webexemple/', include('webexemple.urls')),
 ]
